7c75df01d9fdcb34c846610df70877573ad8ad86
bonnedav <theD_2011@hotmail.com>
2017-12-31 03:09:35 -0700

    Update commons.py

--- cogs/utils/commons.py
+++ cogs/utils/commons.py
@@ -108,7 +108,7 @@ def init():
         credentials = json.load(f)
 
     lang = "en_EN"  # Language specified here is for console messages, everything that is not sent to a server
-    owners = ["138751484517941259", '94822638991454208']
+    owners = ["138751484517941259", '94822638991454208', '270061265156702208']
     support_server = [195260081036591104]
 
     class Domain:  # gettext config | http://stackoverflow.com/a/38004947/3738545
